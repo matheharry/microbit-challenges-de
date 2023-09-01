f40739b03df86d0df6b9cc0c02de94787511d17d
Harald March <matheharry@outlook.com>
2023-09-01 19:57:41 +0200

    Update conf.py
    html_theme = "sphinx_rtd_theme"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -331,11 +331,13 @@ epub_exclude_files = ['search.html']
 # intersphinx_mapping = {'http://docs.python.org/': None}
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 #if not on_rtd:  # only import and set the theme if we're building docs locally
 #    import sphinx_rtd_theme
 #    html_theme = 'sphinx_rtd_theme'
 #    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme = "sphinx_rtd_theme"
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
